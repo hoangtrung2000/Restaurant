b508b7ad02e6301a7fe03c5c7e82dbcf67d8aca6
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2021-11-19 08:58:47 +0700

    Add Order, Review, Footer

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+
 window.onscroll = () =>{
+
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+    let top = window. scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top => offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+  });
 }
 
 document.querySelector("#search-icon").onclick = () =>{
@@ -29,4 +49,28 @@ var swiper = new Swiper(".home-slider", {
     clickable: true,
   },
   loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    640:{
+      slidesPerView: 2,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    1024:{
+      slidesPerView: 3,
+    },
+  },
 });
